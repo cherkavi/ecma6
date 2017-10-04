da920f3d59127f9d8a227fc4e1d4891df9e22615
technik <technik7_job@mail.ru>
2017-10-04 06:31:18 +0200

    change name of the generator with delegate inside

--- 12-generators.js
+++ 12-generators.js
@@ -11,14 +11,14 @@ for(let value of listOfValues()){
 	console.log(`next value from generator after second execution:  ${value}`);
 }
 
-function* generatorWrapper(){
+function* generatorWithDelegate(){
 	yield 1;
 	yield 2;
 	yield *listOfValues(); // yield* listOfValues()
 }
 
 
-let iterator = generatorWrapper();
+let iterator = generatorWithDelegate();
 console.log(`extended iterator next value:  ${JSON.stringify(iterator.next())}`);
 console.log(`extended iterator next value:  ${JSON.stringify(iterator.next())}`);
 console.log(`extended iterator next value:  ${JSON.stringify(iterator.next())}`);
